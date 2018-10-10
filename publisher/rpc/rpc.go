d33a3ede44f64c37ef32272ba094248f7f930036
aarjan baskota <aarjan.baskota@dragonlaw.com.hk>
2018-10-10 16:38:52 +0545

    add rpc pub/sub

---
+++ publisher/rpc/rpc.go
@@ -0,0 +1,112 @@
+/**
+* In RPC, a client sends request message and the server responds with response message.
+* In order to recieve the response we need to send a 'callback' queue address with the request.
+* AMQP protocol predefines 14 properties that go with message.
+* Some of the important ones are:
+* persistent: Marks a message persistent.
+* content_type: Describe the mime-type of encoding.
+* reply_to: Commonly used to name the callback queue.
+* correlation_id: Used to correlate RPC responses with requests.
+
+* In general, our RPC will work like this:
+* For a RPC request, the client sends a message with two properties:
+* 	1. reply_to: which is the callback queue
+*	2. correlation_id: unique value for each request
+* The request is sent to `rpc_queue` queue.
+* The RPC worker/server is waiting for the request on that queue. It checks the `correlation_id` property.
+* If matched, it returns the response on the callback queue.
+ */
+
+package main
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s, error: %+v", msg, err)
+	}
+}
+
+// We use a simple fibonacci generator as method that processes the input and returns a result
+func fib(n int) int {
+	if n == 0 {
+		return 1
+	} else if n == 1 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	// Create connection
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	failOnError(err, "failed to start server")
+	defer conn.Close()
+
+	// Create channel
+	ch, err := conn.Channel()
+	failOnError(err, "failed to create channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"rpc_queue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "failed to declare queue")
+
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "failed to set Qos")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "failed to establish consumer")
+
+	forever := make(chan struct{})
+
+	go func() {
+		for m := range msgs {
+			n, err := strconv.Atoi(string(m.Body))
+			failOnError(err, "invalid integer")
+
+			response := fib(n)
+			log.Printf(" [.] fib(%d): %d", n, response)
+
+			err = ch.Publish(
+				"",        // exchange
+				m.ReplyTo, // routing key
+				true,      // mandatory
+				false,     // immediate
+				amqp.Publishing{
+					CorrelationId: m.CorrelationId,
+					ContentType:   "text/plain",
+					Body:          []byte(strconv.Itoa(response)),
+				},
+			)
+			failOnError(err, "failed to publish message")
+
+			m.Ack(false)
+		}
+	}()
+
+	<-forever
+}
---
+++ receiver/rpc/rpc.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s, error: %v", msg, err)
+	}
+}
+
+func randomString() string {
+	return strconv.FormatInt(time.Now().UnixNano(), 36)
+}
+
+func randInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func fibRPC(n int) (res int) {
+	// create connection
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	failOnError(err, "failed to start server")
+	defer conn.Close()
+
+	// create channel
+	ch, err := conn.Channel()
+	failOnError(err, "failed to create channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	failOnError(err, "failed to create a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto ack
+		false,  // exclusive
+		false,  // no local
+		false,  // no wait
+		nil,    // arguments
+	)
+	failOnError(err, "failed to register a consumer")
+
+	corrID := randomString()
+	err = ch.Publish(
+		"",          // exchange
+		"rpc_queue", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			CorrelationId: corrID,
+			ReplyTo:       q.Name,
+			ContentType:   "text/plain",
+			Body:          []byte(strconv.Itoa(n)),
+		},
+	)
+	failOnError(err, "failed to publish")
+
+	for m := range msgs {
+		if corrID == m.CorrelationId {
+			res, err = strconv.Atoi(string(m.Body))
+			failOnError(err, "failed to convert to integer")
+			break
+		}
+	}
+	return
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	n := bodyForm(os.Args)
+	log.Printf(" [x] Requesting fib(%d)", n)
+	log.Printf(" [.] Got %d", fibRPC(n))
+}
+
+func bodyForm(args []string) int {
+	s := "30"
+	if len(args) == 2 && args[1] != "" {
+		s = args[1]
+	}
+	n, err := strconv.Atoi(s)
+	failOnError(err, "failed to convert to integer")
+	return n
+}
